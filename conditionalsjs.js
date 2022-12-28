f6a729effd14ef2671ca8aee97c066a1d971117a
justin747 <justin747@gmail.com>
2022-12-27 19:59:44 -0800

    Initial commit

---
+++ conditionalsjs.js
@@ -0,0 +1,15 @@
+let caliNBA = ["Clippers", "Warriors", "Kings"]
+
+let caliNFL = ["49ers", "Chargers", "Rams"]
+
+console.log(caliNBA)
+
+let caliSports = caliNBA.concat(caliNFL)
+
+console.log(caliSports)
+
+console.log(caliNBA)
+
+console.log(caliNFL)
+
+caliNBA.includes("Kings")

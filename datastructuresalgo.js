8a9de4f10e425d83e06b3580c0b657e7f5de8fdc
justin747 <justin747@gmail.com>
2023-01-17 19:56:21 -0800

    Big O Notes
    Added Big O Runtimes as notes for study

---
+++ datastructuresalgo.js
@@ -0,0 +1,15 @@
+//Array Big O Operations
+
+// Push = O(1)
+// Pop = O(1)
+
+// Shift = O(N)
+// Unshift = O(N)
+
+// Concat = O(N)
+// Slice = O(N)
+// Splice = O(N)
+
+// Sort = O(N * log N)
+
+// forEach, Map, Filter, Reduce, etc = O(N)

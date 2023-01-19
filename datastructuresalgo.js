d503a6cde8a4811fadccfaeffbc03a677859a82b
justin747 <justin747@gmail.com>
2023-01-18 20:32:50 -0800

    Added Notes
    Added notes on how to break down DS&A problems

--- datastructuresalgo.js
+++ datastructuresalgo.js
@@ -13,3 +13,11 @@
 // Sort = O(N * log N)
 
 // forEach, Map, Filter, Reduce, etc = O(N)
+
+// DS&A Strategies
+
+// 1. Understand the Problem
+// 2. Explore Concrete Examples
+// 3. Break It Down
+// 4. Solve and Simplify
+// 5. Look Back & Refactor

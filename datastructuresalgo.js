20f11c1560d8b370dd1d2787dba91eb9cf9e9a7d
justin747 <justin747@gmail.com>
2023-01-19 21:39:04 -0800

    Wrote a function that takes a string and returns an object with the letters and the number of their occurrences.
    Wrote a function that takes a string and returns an object with the letters and the number of their occurrences.

--- datastructuresalgo.js
+++ datastructuresalgo.js
@@ -21,3 +21,20 @@
 // 3. Break It Down
 // 4. Solve and Simplify
 // 5. Look Back & Refactor
+
+//Write a function that takes a string and returns an object with the letters and the number of their occurrences.
+
+function charCount(str) {
+    var result = {};
+
+    for(var i = 0; i < str.length; i++) {
+        var char = str[i];
+        if (result[char] > 0) {
+            result[char]++;
+        } else {
+            result[char] = 1;
+        };
+    }
+
+    return result;
+}
